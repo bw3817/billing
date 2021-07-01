21e910af7459858128b8c0dc569e5ebf42e4dbd5
Brian Wolf <bw@activustech.com>
2021-07-01 09:50:06 -0400

    Python module to add hours to a project (Bacon Sails by default)

---
+++ addhours.py
@@ -0,0 +1,61 @@
+"""Add billable hours. Defaults to Bacon Sails & Marine Supplies"""
+
+import decimal
+from datetime import datetime, date
+
+from sqlalchemy  import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+from models.invoices import Hours
+from instance.development import SQLALCHEMY_DATABASE_URI
+
+
+def get_hours():
+    prompt = 'Enter hours: '
+    while True:
+        response = input(prompt)
+        try:
+            return decimal.Decimal(response)
+        except decimal.InvalidOperation:
+            pass
+
+
+def get_date():
+    prompt = f'Enter date [{date.today()}]: '
+    while True:
+        response = input(prompt).strip()
+        if response == '':
+            return date.today()
+
+        try:
+            return date.fromisoformat(response)
+        except ValueError:
+            pass
+
+
+def add_hours(db_session, cust_id=2, project_id=22):
+    hrs = get_hours()
+    performed = get_date()
+    new_hours = Hours()
+    new_hours.cust_id = cust_id
+    new_hours.project_id = project_id
+    new_hours.hrs = hrs
+    new_hours.performed = performed
+    new_hours.billing_status = 'U'
+    new_hours.cre_dt = datetime.now()
+    new_hours.mod_dt = datetime.now()
+    db_session.add(new_hours)
+    db_session.commit()
+    return new_hours
+
+
+def get_session():
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    SessionFactory = sessionmaker(bind=engine)
+    return SessionFactory()
+
+
+if __name__ == '__main__':
+    db_session = get_session()
+    new_hours = add_hours(db_session)
+    print(new_hours)
